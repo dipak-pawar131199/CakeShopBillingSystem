08c6e32297a481633f6c3815b83eb82cb8b26470
Dipak Rajendra Pawar <77726578+dipak-pawar131199@users.noreply.github.com>
2021-11-12 12:43:25 +0530

    Add files via upload

---
+++ CakeShopBillingSystem/src/cakeshopmanagementsystem/CakeShopManagementSystem.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cakeshopmanagementsystem;
+
+/**
+ *
+ * @author dipak
+ */
+public class CakeShopManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+        loginpage l=new loginpage();
+        l.setVisible(true);
+        
+ 
+    }
+      
+}
